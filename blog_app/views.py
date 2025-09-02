0bebf0040c474a50a78c79aae150cf9651e6d97f
Dev <nameofdeev@gmail.com>
2025-09-02 22:20:53 +0545

    Added View More Functionality with updates regarding userinterface

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog_app.models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(
+        request,
+        "post_list.html",
+        {"posts": posts},
+    )
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(
+        request,
+        "post_detail.html",
+        {"post": post},
+    )
